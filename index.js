fc0d154ee0d7dce317edf704e89817216e30dde1
Peter Horn <pdhorn@gmail.com>
2020-12-28 09:01:44 -0600

    module stuff figured out

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+const yargs = require("yargs");
+// const controller = require("./lib/controller");
+import { analyze } from "./lib/controller.js";
+
+const argv = yargs
+  .option("files", {
+    alias: "f",
+    description: "pass filenames",
+    type: "array",
+  })
+  .help()
+  .alias("help", "h").argv;
 
 clear();
 
 console.log(
   chalk.yellow(figlet.textSync("Hook-navigator", { horizontalLayout: "full" }))
 );
+
+argv.files.forEach((filePath) => {
+  console.log(analyze(filePath));
+});
--- readme.md
+++ readme.md
@@ -2,6 +2,10 @@
 
 Untangle the web of state setters in React.
 
+## Node versions
+
+Works in Node 12 and 14
+
 ## Install
 
 `npm install`
